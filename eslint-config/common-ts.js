09c78386e504e80dc3513c394a8e126fd28d45f3
Han Il-u <rageatm@alumni.kaist.ac.kr>
2023-07-12 13:06:59 +0900

    feat: eslint config 추가

---
+++ eslint-config/common-ts.js
@@ -0,0 +1,16 @@
+// based on pebbles/.eslintrc.js
+module.exports = {
+  env: { jest: true },
+  extends: ['plugin:prettier/recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: '2021', sourceType: 'module' },
+  plugins: ['import', '@typescript-eslint'],
+  root: true,
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    'import/newline-after-import': 'error',
+    'no-console': ['error', { allow: ['warn'] }],
+    'eol-last': 'error',
+  },
+};
---
+++ eslint-config/react-ts.js
@@ -0,0 +1,15 @@
+// based of coloso/backoffice/.eslintrc
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: { browser: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
