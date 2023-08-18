703be314ae6a7996be8af9f836c2f9b90180b3d7
Han Il-u <rageatm@alumni.kaist.ac.kr>
2023-08-18 14:32:37 +0900

    fix: eslint-config parserOptions lib 수정

--- eslint-config/common-ts.js
+++ eslint-config/common-ts.js
@@ -3,7 +3,7 @@ module.exports = {
   env: { jest: true, node: true },
   extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: '2021', lib: ['2021'], sourceType: 'module' },
+  parserOptions: { ecmaVersion: 2021, lib: ['es2021'], sourceType: 'module' },
   plugins: ['import', '@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'warn',
