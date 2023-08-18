6427e8fa14317c944f79cfa2fae27f6bb2f44129
Han Il-u <rageatm@alumni.kaist.ac.kr>
2023-08-18 13:24:55 +0900

    feat: eslint 설정 수정

--- eslint-config/common-ts.js
+++ eslint-config/common-ts.js
@@ -1,15 +1,15 @@
 module.exports = {
-  env: { jest: true },
-  extends: ['plugin:prettier/recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: '2021', sourceType: 'module' },
-  plugins: ['import', '@typescript-eslint'],
   root: true,
+  env: { jest: true, node: true },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: '2021', lib: ['2021'], sourceType: 'module' },
+  plugins: ['import', '@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-namespace': 'off',
     'import/newline-after-import': 'error',
-    'no-console': ['error', { allow: ['warn'] }],
+    'no-console': ['error'],
     'eol-last': 'error',
   },
 };
